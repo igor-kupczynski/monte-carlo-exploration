0515fcb80c3b06d5d7a775fab91fa1a27fd16546
Igor Kupczyński <igor@kupczynski.info>
2020-05-04 11:26:46 +0200

    Add a simple statistical framework

---
+++ stats/stats.go
@@ -0,0 +1,70 @@
+// Package stats offers convenience functions to calculate descriptive result properties
+package stats
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+var wantPercentiles = []int{1, 5, 10, 25, 50, 75, 90, 95, 99}
+
+// Summary represents the basic statistics on the given dataset
+type Summary struct {
+	Total    int
+	Baseline int64
+	Avg      float64
+	Min, Max int64
+	// Percentage of the dataset below, above, and at the baseline
+	Below, At, Above float64
+	Percentiles      map[int]int64
+}
+
+func (s *Summary) String() string {
+	var buf strings.Builder
+
+	buf.WriteString(fmt.Sprintf("Dataset [len=%d, baseline=%d]\n", s.Total, s.Baseline))
+	buf.WriteString(fmt.Sprintf("* Avg=%f\tMin=%d\tMax%d\n", s.Avg, s.Min, s.Max))
+	buf.WriteString(fmt.Sprintf("* %% of items below=%f%%\tat=%f%%\tabove=%f%% baseline\n", s.Below, s.At, s.Above))
+	buf.WriteString(fmt.Sprintf("* Percentiles:\n"))
+
+	for _, p := range wantPercentiles {
+		buf.WriteString(fmt.Sprintf("\t- p%02d%%: $%d\n", p, s.Percentiles[p]))
+	}
+
+	return buf.String()
+}
+
+// Describe provides basic statistics on the given dataset.
+//
+// The dataset will be sorted in place
+func Describe(results []int64, baseline int64) *Summary {
+	total := len(results)
+	Int64Slice(results).Sort()
+
+	var sum int64
+	for _, x := range results {
+		sum += x
+	}
+	avg := float64(sum) / float64(total)
+
+	firstAt := sort.Search(total, func(i int) bool { return results[i] >= baseline })
+	firstAbove := sort.Search(total, func(i int) bool { return results[i] > baseline })
+
+	percentiles := make(map[int]int64)
+	for _, p := range wantPercentiles {
+		percentiles[p] = results[p*total/100]
+	}
+
+	return &Summary{
+		Total:       total,
+		Baseline:    baseline,
+		Min:         results[0],
+		Max:         results[total-1],
+		Avg:         avg,
+		Below:       100 * float64(firstAt) / float64(total),
+		At:          100 * float64(firstAbove-firstAt) / float64(total),
+		Above:       100 * float64(total-firstAbove) / float64(total),
+		Percentiles: percentiles,
+	}
+}
---
+++ stats/stats_test.go
@@ -0,0 +1,142 @@
+package stats
+
+import (
+	"reflect"
+	"testing"
+)
+
+type dataset struct {
+	results  []int64
+	baseline int64
+}
+
+func TestDescribe(t *testing.T) {
+	tests := []struct {
+		name    string
+		dataset dataset
+		want    *Summary
+	}{
+		{
+			name: "simplified example",
+			dataset: dataset{
+				results:  []int64{10},
+				baseline: 10,
+			},
+			want: &Summary{
+				Total:    1,
+				Baseline: 10,
+				Min:      10,
+				Max:      10,
+				Avg:      10.0,
+				Below:    0.0,
+				At:       100.0,
+				Above:    0.0,
+				Percentiles: map[int]int64{
+					1:  10,
+					5:  10,
+					10: 10,
+					25: 10,
+					50: 10,
+					75: 10,
+					90: 10,
+					95: 10,
+					99: 10,
+				},
+			},
+		},
+		{
+			name:    "increasing capital",
+			dataset: incCap(),
+			want: &Summary{
+				Total:    100,
+				Baseline: 50,
+				Min:      0,
+				Max:      99,
+				Avg:      49.5,
+				Below:    50.0,
+				At:       1.0,
+				Above:    49.0,
+				Percentiles: map[int]int64{
+					1:  1,
+					5:  5,
+					10: 10,
+					25: 25,
+					50: 50,
+					75: 75,
+					90: 90,
+					95: 95,
+					99: 99,
+				},
+			},
+		},
+		{
+			name:    "30% ruined",
+			dataset: ruined30(),
+			want: &Summary{
+				Total:    100,
+				Baseline: 10,
+				Min:      0,
+				Max:      10,
+				Avg:      7,
+				Below:    30.0,
+				At:       70.0,
+				Above:    0.0,
+				Percentiles: map[int]int64{
+					1:  0,
+					5:  0,
+					10: 0,
+					25: 0,
+					50: 10,
+					75: 10,
+					90: 10,
+					95: 10,
+					99: 10,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Describe(tt.dataset.results, tt.dataset.baseline); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Describe() = %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
+
+// incCap returns 100 samples with increasing capital $0 to $99, the baseline being $50
+func incCap() dataset {
+	total := 100
+	results := make([]int64, total)
+
+	for i := 0; i < total; i++ {
+		results[i] = int64(i)
+	}
+
+	return dataset{
+		results:  results,
+		baseline: 50,
+	}
+}
+
+// ruined30 returns 100 samples: 30% is ruined and 70% has initial capital $10
+func ruined30() dataset {
+	total := 100
+	ruined := 30
+	initial := int64(10)
+
+	results := make([]int64, total)
+
+	for i := 0; i < total; i++ {
+		var captial int64
+		if i >= ruined {
+			captial = initial
+		}
+		results[i] = captial
+	}
+
+	return dataset{
+		results:  results,
+		baseline: initial,
+	}
+}
